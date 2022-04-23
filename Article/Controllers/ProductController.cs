2f18a9d56967f098023b6e75f113097efe273c5e
Sefa Dudu <77778888+SefaDudu@users.noreply.github.com>
2022-04-23 18:36:26 +0300

    search entegre edildi

--- Article/Controllers/ProductController.cs
+++ Article/Controllers/ProductController.cs
@@ -5,32 +5,52 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Linq;
 using Dapper;
+using Article.ViewsModel;
+using System;
+
 
 namespace Article.Controllers
 {
     public class ProductController : Controller
     {
         Context context = new Context();
+        
 
-
-        public async Task<IActionResult> Index(string id)
+        public async Task<IActionResult> Index(string? id,  string? word)
         {
             var conn = context.Database.GetDbConnection();
-            var query = "select * from Products where CategoryId= " + id;
-            IEnumerable<Product> product;
+            await conn.OpenAsync();
+
+            ProductVm productVm = null;
+            IEnumerable<Product> product = null;
+            var query = "";
+
+            if (word == null && id != null)
+            {
+                query = "select * from Products where CategoryId= " + id;
+            }
+            else
+            {
+                query = $"select * from Products WHERE Name LIKE '%{word}%'";
+            }
 
             try
             {
-                await conn.OpenAsync();
-
                 product = await conn.QueryAsync<Product>(query);
+                productVm = new ProductVm()
+                {
+                    Product = product,
+                    word = word,
+                    count = product.Count()
+                };
             }
 
             finally
             {
                 conn.Close();
             }
-            return View(product);
+
+            return View(productVm);
         }
 
         public IActionResult ProductDetail(int id)
---
+++ Article/ViewsModel/ProductVm.cs
@@ -0,0 +1,16 @@
+﻿using Article.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Article.ViewsModel
+{
+    public class ProductVm
+    {
+        public IEnumerable<Product>? Product { get; set; }
+
+        public int? count  { get; set; }
+        public string? word  { get; set; }
+    }
+}
